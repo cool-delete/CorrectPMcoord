a5058c48079c7c0e01412816575cc3d19d5b6471
cool-delete <kk520149492@gmail.com>
2016-07-23 16:23:20 +0800

    upload app

---
+++ app/src/main/java/cool_delete/correctpmcoord/correctUit.java
@@ -0,0 +1,39 @@
+package cool_delete.correctpmcoord;
+
+/**
+ * Created by Administrator on 2016/7/23.
+ */
+public class correctUit {
+
+    private static double pi = 3.14159265358979324;
+    public static double[] transform(double wgLat, double wgLon) {
+
+        double dLat = transformLat(wgLon - 105.0, wgLat - 35.0);
+        double dLon = transformLon(wgLon - 105.0, wgLat - 35.0);
+        double radLat = wgLat / 180.0 * pi;
+        double magic = Math.sin(radLat);
+        double ee = 0.00669342162296594323;
+        magic = 1 - ee * magic * magic;
+        double sqrtMagic = Math.sqrt(magic);
+        double a = 6378245.0;
+        dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * pi);
+        dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * pi);
+
+        return new double[]{wgLat + dLat, wgLon + dLon};
+    }
+    private static double transformLon(double x, double y) {
+        double ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(x * pi) + 40.0 * Math.sin(x / 3.0 * pi)) * 2.0 / 3.0;
+        ret += (150.0 * Math.sin(x / 12.0 * pi) + 300.0 * Math.sin(x / 30.0 * pi)) * 2.0 / 3.0;
+        return ret;
+    }
+
+    private static double transformLat(double x, double y) {
+        double ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x));
+        ret += (20.0 * Math.sin(6.0 * x * pi) + 20.0 * Math.sin(2.0 * x * pi)) * 2.0 / 3.0;
+        ret += (20.0 * Math.sin(y * pi) + 40.0 * Math.sin(y / 3.0 * pi)) * 2.0 / 3.0;
+        ret += (160.0 * Math.sin(y / 12.0 * pi) + 320 * Math.sin(y * pi / 30.0)) * 2.0 / 3.0;
+        return ret;
+    }
+}
---
+++ app/src/main/java/cool_delete/correctpmcoord/xposed.java
@@ -0,0 +1,166 @@
+package cool_delete.correctpmcoord;
+
+import android.location.Location;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+/**
+ * Created by Administrator on 2016/7/14.
+ */
+public class xposed implements IXposedHookLoadPackage {
+
+    private Class<?> loc;
+    private Class<?> UpsightLocationTracker_data;
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
+        if (loadPackageParam.packageName.equals("com.nianticlabs.pokemongo")) {
+            XposedBridge.log("已开启pg");
+            XposedHelpers.findAndHookMethod("com.nianticlabs.nia.location.NianticLocationManager", loadPackageParam.classLoader, "locationUpdate",
+                    Location.class,
+                    int[].class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                        XposedBridge.log("精度: ");
+                    if (param.args[0] instanceof Location) {
+
+                        double longitude = ((Location) param.args[0]).getLongitude();
+                        double latitude = ((Location) param.args[0]).getLatitude();
+//                        param.setResult(param.getResult());
+                        XposedBridge.log("精度: " + longitude + "纬度: " + latitude);
+                        Location old = (Location) param.args[0];
+                        double[] loct = correctUit.transform(old.getLatitude(), old.getLongitude());
+                        old.setLatitude(loct[0]);
+                        old.setLongitude(loct[1]);
+//                        param.args[0] = old;
+                        XposedBridge.log("已改");
+                    }
+                }
+            });
+//            XposedHelpers.findAndHookMethod("com.nianticlabs.nia.location.NianticLocationManager", loadPackageParam.classLoader, "nativeLocationUpdate", Location.class, int[].class, new XC_MethodHook() {
+//                @Override
+//                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//
+//                    if (param.getResult() instanceof Location) {
+////                        double longitude = ((Location) param.getResult()).getLongitude();
+////                        double latitude = ((Location) param.getResult()).getLatitude();
+////                        param.setResult(param.getResult());
+//                        XposedBridge.log("com.nianticlabs.nia.location.nativeLocationUpdate本地传来经纬度了");
+//                    }
+//                }
+//            });
+            XposedHelpers.findAndHookMethod("com.nianticlabs.nia.location.LocationManagerProvider", loadPackageParam.classLoader, "updateLocation", Location.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                    if (param.getResult() instanceof Location) {
+//                        double longitude = ((Location) param.getResult()).getLongitude();
+//                        double latitude = ((Location) param.getResult()).getLatitude();
+//                        param.setResult(param.getResult());
+                        XposedBridge.log("com.nianticlabs.nia.location.LocationManagerProvider的updateLocation传来经纬度了");
+                    }
+                }
+            });
+            XposedHelpers.findAndHookMethod("com.unity3d.player.r", loadPackageParam.classLoader, "a", Location.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                    if (param.getResult() instanceof Location) {
+//                        double longitude = ((Location) param.getResult()).getLongitude();
+//                        double latitude = ((Location) param.getResult()).getLatitude();
+//                        param.setResult(param.getResult());
+                        XposedBridge.log("com.unity3d.player.r一个参数传来经纬度了");
+                    }
+                }
+            });
+            XposedHelpers.findAndHookMethod("com.unity3d.player.r", loadPackageParam.classLoader, "a", Location.class, Location.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                    if (param.getResult() instanceof Location) {
+//                        double longitude = ((Location) param.getResult()).getLongitude();
+//                        double latitude = ((Location) param.getResult()).getLatitude();
+//                        param.setResult(param.getResult());
+                        XposedBridge.log("com.unity3d.player.r二个参数传来经纬度了");
+                    }
+                }
+            });
+
+            XposedHelpers.findAndHookMethod("crittercism.android.bc", loadPackageParam.classLoader, "a", Location.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                        XposedBridge.log("Bc传来经纬度了");
+                    if (param.args[0] instanceof Location) {
+
+                        double longitude = ((Location) param.args[0]).getLongitude();
+                        double latitude = ((Location) param.args[0]).getLatitude();
+//                        param.setResult(param.getResult());
+                        XposedBridge.log("Bc传来经纬度了"+"精度: "+longitude+"纬度: "+latitude);
+                    }
+                }
+            });
+            XposedHelpers.findAndHookMethod("crittercism.android.bc", loadPackageParam.classLoader, "a", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                    if (param.getResult() instanceof Location) {
+//                        double longitude = ((Location) param.getResult()).getLongitude();
+//                        double latitude = ((Location) param.getResult()).getLatitude();
+//                        param.setResult(param.getResult());
+                        XposedBridge.log("Bc返回函数传来经纬度了");
+                    }
+                }
+            });
+
+            XposedHelpers.findAndHookMethod("com.crittercism.app.Crittercism", loadPackageParam.classLoader, "updateLocation", Location.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                    if (param.getResult() instanceof Location) {
+//                        double longitude = ((Location) param.getResult()).getLongitude();
+//                        double latitude = ((Location) param.getResult()).getLatitude();
+//                        param.setResult(param.getResult());
+                        XposedBridge.log("com.crittercism.app.Crittercism传来经纬度了");
+                    }
+                }
+            });
+            XposedHelpers.findAndHookMethod("com.google.ads.mediation.MediationAdRequest", loadPackageParam.classLoader, "getLocation", new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+
+                    if (param.getResult() instanceof Location) {
+//                        double longitude = ((Location) param.getResult()).getLongitude();
+//                        double latitude = ((Location) param.getResult()).getLatitude();
+//                        param.setResult(param.getResult());
+                        XposedBridge.log("com.crittercism.app.Crittercism传来经纬度了");
+                    }
+                }
+            });
+//            XposedBridge.hookAllConstructors("com.google.ads.mediation.MediationAdRequest", loadPackageParam.classLoader, Date.class, new XC_MethodHook() {
+//                @Override
+//                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//
+//                    if (param.getResult() instanceof Location) {
+////                        double longitude = ((Location) param.getResult()).getLongitude();
+////                        double latitude = ((Location) param.getResult()).getLatitude();
+////                        param.setResult(param.getResult());
+//                        XposedBridge.log("com.crittercism.app.Crittercism传来经纬度了");
+//                    }
+//                }
+//            });
+//            UpsightLocationTracker_data = loadPackageParam.classLoader.loadClass("com.upsight.android.analytics.provider.UpsightLocationTracker$Data");
+//            XposedHelpers.findAndHookMethod("com.upsight.android.analytics.internal.provider.LocationTracker", loadPackageParam.classLoader, "track", UpsightLocationTracker_data, new XC_MethodHook() {
+//                @Override
+//                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                    param.
+//                }
+//            });
+        }
+    }
+}
